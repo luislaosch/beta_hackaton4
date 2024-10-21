ffa0c504793e58d78c09c1c8be839a15d09d8137
Luis Laos <SV46552638@idat.pe>
2024-10-21 00:12:43 -0500

    avance r2 ejercicio 11

--- script2.js
+++ script2.js
@@ -59,7 +59,7 @@ const cadenaRandom = () => {
 document.getElementById("r2_e3_btn1").addEventListener("click", (varTipo) => {
     varTipo.preventDefault();
     const azar = enteroRandom();
-    var random=null;
+    var random = null;
     switch (azar % 3) {
         case 1:
             random = enteroRandom();
@@ -72,10 +72,10 @@ document.getElementById("r2_e3_btn1").addEventListener("click", (varTipo) => {
             random = cadenaRandom();
             break;
     }
-    document.getElementById("r2_e3_span1").innerText=`${random}`;
+    document.getElementById("r2_e3_span1").innerText = `${random}`;
     console.log(typeof random);
-    document.getElementById("r2_e3_resultado").innerText=`${(typeof random)}`;
-    random=null;
+    document.getElementById("r2_e3_resultado").innerText = `${(typeof random)}`;
+    random = null;
 });
 
 //Ejercicio 04
@@ -83,63 +83,163 @@ const sumaParametros = (...rest) => {
     const sum = (accumulator, currentValue) => accumulator + currentValue;
     return rest.reduce(sum, 0);
 };
-document.getElementById("r2_e4_btn1").addEventListener("click",(paramRest)=>{
+document.getElementById("r2_e4_btn1").addEventListener("click", (paramRest) => {
     paramRest.preventDefault();
     let numeros = [];
     for (let index = 0; index < 10; index++) {
         numeros.push(enteroRandom());
     }
-    document.getElementById("r2_e4_span1").innerText=`${numeros}`;
-    document.getElementById("r2_e4_resultado").innerHTML=`${sumaParametros(...numeros)}`;
+    document.getElementById("r2_e4_span1").innerText = `${numeros}`;
+    document.getElementById("r2_e4_resultado").innerHTML = `${sumaParametros(...numeros)}`;
 })
 
 //Ejercicio 05
-const sinCadenas = (arreglo=[])=>{
-    let sinCadenas=[];
+const sinCadenas = (arreglo = []) => {
+    let sinCadenas = [];
     arreglo.forEach(element => {
-        if(!isNaN(element)){
+        if (!isNaN(element)) {
             sinCadenas.push(element);
         }
     });
-    return sinCadenas; 
+    return sinCadenas;
 }
-document.getElementById("r2_e5_btn1").addEventListener("click",(filtarCadenas)=>{
+document.getElementById("r2_e5_btn1").addEventListener("click", (filtarCadenas) => {
     filtarCadenas.preventDefault();
     //generar arreglo aleatorio
-    let arreglo =[]
+    let arreglo = []
     for (let index = 0; index < 12; index++) {
-        if(enteroRandom()% 2 === 0){
+        if (enteroRandom() % 2 === 0) {
             arreglo.push(enteroRandom());
-        }else{
+        } else {
             arreglo.push(cadenaRandom());
         }
     }
-    document.getElementById("r2_e5_span1").innerText=`${arreglo}`;
-    document.getElementById("r2_e5_resultado").innerText=`${sinCadenas(arreglo)}`;
+    document.getElementById("r2_e5_span1").innerText = `${arreglo}`;
+    document.getElementById("r2_e5_resultado").innerText = `${sinCadenas(arreglo)}`;
 })
 
 // ejercicio 06
 
 const minMaxSimple = (arreglo) => {
     // console.log("minmaxSample ",arreglo);
-    let max = Math.min(...arreglo);
-    let min = Math.max(...arreglo);
+    let min = Math.min(...arreglo);
+    let max = Math.max(...arreglo);
     // console.log(min,max);
     return [max, min];
 };
 
-document.getElementById("r2_e6_btn1").addEventListener("click",(minMax)=>{
+document.getElementById("r2_e6_btn1").addEventListener("click", (minMax) => {
     minMax.preventDefault();
     //generar arreglo
-    let arreglo =[]
+    let arreglo = []
 
     for (let index = 0; index < 12; index++) {
         arreglo.push(enteroRandom());
     }
-    document.getElementById("r2_e6_span1").innerText=`${arreglo}`;
-    const arr =minMaxSimple(arreglo);
+    document.getElementById("r2_e6_span1").innerText = `${arreglo}`;
+    const arr = minMaxSimple(arreglo);
     // console.log(arr);
-    document.getElementById("r2_e6_resultado").innerText = 
+    document.getElementById("r2_e6_resultado").innerText =
         `Mínimo: ${arr[0]}, Máximo: ${arr[1]}`;
 
+})
+
+// ejercicio 07
+const formartPhoneNumber = (numero = []) => {
+    if (numero.length != 10) {
+        return "numero invalido";
+    }
+
+    let part1 = numero.slice(0, 3);
+    part1 = part1.join("");
+
+    let part2 = numero.slice(3, 6).join("");
+    let part3 = numero.slice(6, 10).join("");
+
+    return `(${part1}) ${part2}-${part3}`;
+};
+
+document.getElementById("r2_e7_btn1").addEventListener("click", (numTelf) => {
+    numTelf.preventDefault();
+    let arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    document.getElementById("r2_e7_span1").innerText = `${arreglo}`;
+    document.getElementById("r2_e7_resultado").innerText = `${formartPhoneNumber(arreglo)}`;
+})
+
+//ejercicio 08
+
+const formartoMatrizMatrices = (matriz = []) => {
+    // console.log(matriz);
+    let arr = [];
+    matriz.forEach(element => {
+        let max = Math.max(...element);
+        arr.push(max);
+    });
+    return arr;
+}
+
+document.getElementById("r2_e8_btn1").addEventListener("click", (mayorMatrices) => {
+    indiceCaracter.preventDefault();
+    const findLargestNums = [[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]];
+    // console.log(findLargestNums);
+    document.getElementById("r2_e8_span1").innerText = `${findLargestNums}`;
+    document.getElementById("r2_e8_resultado").innerText = `${formartoMatrizMatrices(findLargestNums)}`;
+})
+
+//ejercicio 09
+const validaCadenaRelativo = (cadena, minimo, maximo) => {
+    let validacion = ((cadena.length > minimo) && (cadena.length < maximo)) ? true : false;
+    return validacion;
+}
+
+const encontrarIndices = (cadena, letra) => {
+    if (cadena.indexOf(letra) === -1) {
+        return "Letra no encontrada en la cadena";
+    } else {
+        return [cadena.indexOf(letra), cadena.lastIndexOf(letra)];
+    }
+}
+
+document.getElementById("r2_e9_btn1").addEventListener("click", (indiceCaracter) => {
+    indiceCaracter.preventDefault();
+    const cadena = document.getElementById("r2_e9_input1").value;
+    const letra = document.getElementById("r2_e9_input2").value;
+
+    if (validaCadenaRelativo(cadena, 0, 20) && validaCadenaRelativo(letra, 0, 2)) {
+        document.getElementById("r2_e9_resultado").innerText = `${encontrarIndices(cadena, letra)}`;
+    } else {
+        alert("Ejercicio 9, Valores no son validos");
+    }
+})
+
+// ejercicio 10
+const toArray = (objeto = {}) => {
+    return Object.entries(objeto);
+};
+document.getElementById("r2_e10_btn1").addEventListener("click", (convertirArreglo) => {
+    convertirArreglo.preventDefault();
+
+    const objObejto = {
+        a: 1,
+        b: 2
+    };
+    // document.getElementById("r2_10_span1").innerText = `${objObejto.a} ${objObejto.b}`;
+    document.getElementById("r2_e10_resultado").innerText = `${toArray(objObejto)}`;
+})
+const sumarPresupuestos =(individuos ={})=>{
+    let accumulator=0;
+    individuos.forEach(individuo => { accumulator+=individuo.budget;});
+    return accumulator;
+}
+//ejercicio 11
+document.getElementById("r2_e11_btn1").addEventListener("click", (ejercicio11) => {
+    ejercicio11.preventDefault();
+
+    const getBudgets= ([
+        { name: "John", age: 21, budget: 23000 },
+        { name: "Steve", age: 32, budget: 40000 },
+        { name: "Martin", age: 16, budget: 2700 }
+        ]);
+    // document.getElementById("r2_10_span1").innerText = `${objObejto.a} ${objObejto.b}`;
+    document.getElementById("r2_e11_resultado").innerText = `${sumarPresupuestos(getBudgets)}`;
 })
